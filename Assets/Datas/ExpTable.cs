76505634abfa6962b64495094d9a81857a1bc014
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2025-06-13 15:31:57 +0800

    Update 地圖塊_河流
    2025-06-13T15:31:56

--- Assets/Datas/ExpTable.cs
+++ Assets/Datas/ExpTable.cs
@@ -8,7 +8,10 @@ using UnityEngine;
 public class ExpTable : ScriptableObject
 {
     /// <summary>
-    /// 升級所需經驗值
+    /// 升級所需經驗值 <br></br><br></br>
+    /// 這裡使用的成長函數為 : y = 400 * ( 1.3 ^ x ) , 取整數 <br></br>
+    /// x : 等級 <br></br>
+    /// y : 升下一等級需要的經驗值 <br></br>
     /// </summary>
     public float[] MaxExp;
 }
