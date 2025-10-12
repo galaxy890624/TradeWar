ac23687694b3d85b7551842cac36a002a10db47d
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2025-10-12 12:05:15 +0800

    Update 採集
    2025-10-12T12:05:15

---
+++ Assets/Scripts/Collect/WoodCollector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace galaxy890624
+{
+    /// <summary>
+    /// 木頭採集 <br><br></br></br>
+    /// - 掛在木頭的Prefab上 <br></br>
+    /// - 如果玩家碰到木頭，則採集木頭 <br></br>
+    /// - 採集木頭後，木頭消失 <br></br>
+    /// - 玩家獲得木頭數量 +1 <br></br>
+    /// </summary>
+    public class WoodCollector : MonoBehaviour
+    {
+        [SerializeField, Header("玩家物件")] private GameObject Player;
+        [SerializeField, Header("玩家資料")] private PlayerData PlayerData;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject == Player)
+            {
+                // 玩家獲得木頭數量 +1
+                PlayerData.Wood++;
+                // 木頭消失
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
